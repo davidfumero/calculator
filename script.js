227a2a8b747a428025d6bceb9c7413e587460b4e
davidfumero <djfumero18@gmail.com>
2025-05-06 16:58:34 -0400

    Add positive/negative functionality

--- script.js
+++ script.js
@@ -9,29 +9,39 @@ if (display) {
 
 button.forEach(button => {
     button.addEventListener('click', function() {
-        console.log("Button clicked:", this.textContent)
-        if (this.textContent == "AC") {
+        let btnText = this.textContent;
+        console.log("Button clicked:", btnText)
+
+        if (btnText == "AC") {
             display.textContent = "0";
-        } else if (this.textContent == "+/-") {
+        } else if (btnText == "+/-") {
+            if (display.textContent = "0") {
+                //
+            } else if (display.textContent.includes("-")) {
+                let oldText = display.textContent;
+                let newText = oldText.slice(2, -1);
+                display.textContent = newText;
+            } else {
+                display.textContent = "(-" + display.textContent + ")";
+            }
+        } else if (btnText == "%") {
 
-        } else if (this.textContent == "%") {
+        } else if (btnText == "/") {
 
-        } else if (this.textContent == "/") {
+        } else if (btnText == "-") {
 
-        } else if (this.textContent == "-") {
+        } else if (btnText == "+") {
 
-        } else if (this.textContent == "+") {
+        } else if (btnText == "=") {
 
-        } else if (this.textContent == "=") {
+        } else if (btnText == "?") {
 
-        } else if (this.textContent == "?") {
-
-        } else if (this.textContent == ".") {
-            display.textContent.includes(".") ? null : display.textContent = display.textContent + this.textContent;
+        } else if (btnText == ".") {
+            display.textContent.includes(".") ? null : display.textContent = display.textContent + btnText;
         } else if (display.textContent == "0") {
-            display.textContent = this.textContent;
+            display.textContent = btnText;
         } else {
-            display.textContent = display.textContent + this.textContent;
+            display.textContent = display.textContent + btnText;
         }
     })
 })
